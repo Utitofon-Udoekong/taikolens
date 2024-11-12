972f7ec12522fef53c8848441382d5ddec272b52
Utitofon-Udoekong <utitofonudoekong0@gmail.com>
2024-11-12 17:40:57 +0100

    Initialize subgraph

---
+++ subgraph/src/erc-20.ts
@@ -0,0 +1,101 @@
+import {
+  Approval as ApprovalEvent,
+  Initialized as InitializedEvent,
+  MinterAdded as MinterAddedEvent,
+  MinterRemoved as MinterRemovedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent
+} from "../generated/ERC20/ERC20"
+import {
+  Approval,
+  Initialized,
+  MinterAdded,
+  MinterRemoved,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMinterAdded(event: MinterAddedEvent): void {
+  let entity = new MinterAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.minter = event.params.minter
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMinterRemoved(event: MinterRemovedEvent): void {
+  let entity = new MinterRemoved(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.minter = event.params.minter
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ subgraph/tests/erc-20-utils.ts
@@ -0,0 +1,116 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  Initialized,
+  MinterAdded,
+  MinterRemoved,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/ERC20/ERC20"
+
+export function createApprovalEvent(
+  owner: Address,
+  spender: Address,
+  value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return approvalEvent
+}
+
+export function createInitializedEvent(version: i32): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(version))
+    )
+  )
+
+  return initializedEvent
+}
+
+export function createMinterAddedEvent(minter: Address): MinterAdded {
+  let minterAddedEvent = changetype<MinterAdded>(newMockEvent())
+
+  minterAddedEvent.parameters = new Array()
+
+  minterAddedEvent.parameters.push(
+    new ethereum.EventParam("minter", ethereum.Value.fromAddress(minter))
+  )
+
+  return minterAddedEvent
+}
+
+export function createMinterRemovedEvent(minter: Address): MinterRemoved {
+  let minterRemovedEvent = changetype<MinterRemoved>(newMockEvent())
+
+  minterRemovedEvent.parameters = new Array()
+
+  minterRemovedEvent.parameters.push(
+    new ethereum.EventParam("minter", ethereum.Value.fromAddress(minter))
+  )
+
+  return minterRemovedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
